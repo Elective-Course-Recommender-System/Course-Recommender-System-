3f4ad02a7e33d63d5bdede280b8f8e510d3450c9
sid18996 <sid18996@users.noreply.github.com>
2018-09-30 08:20:30 +0530

    Updated prerequisites based grade prediction method

--- code/prerequisites_based_grades_prediction.py
+++ code/prerequisites_based_grades_prediction.py
@@ -1,7 +1,6 @@
 import os
 import openpyxl
 
-#print("Collaborative Filtering Item-Based Algorithm for Grade Prediction")
 wb = openpyxl.load_workbook('data/prerequsite.xlsx')
 wb2 = openpyxl.load_workbook('data/student.xlsx')
 sheetname = wb.get_sheet_names()
@@ -12,11 +11,9 @@ cpi = wb.get_sheet_by_name(sheetname[2])
 weight = wb.get_sheet_by_name(sheetname[0])
 wsheet = wb.get_sheet_by_name(sheetname[1])
 
-# rsheet=wb.get_sheet_by_name(sheetname[0])
 course =[]
 for i in range (2,grade.max_column+1):
     course.append(grade.cell(row=1,column=i).value)
-print(course)
 
 
 for i in range(1,54):
@@ -25,10 +22,8 @@ for i in range(1,54):
         for k in range(2,weight.max_column+1):
             if weight.cell(row=j,column=k).value>0:
                 if grade.cell(row=i+1,column=k).value!= None and grade.cell(row=i+1,column=k).value!= 'XX' and grade.cell(row=i + 1, column=k).value != 'I' and grade.cell(row=i+1,column=k).value!= 'NT':
-                    # print(i+1,k,grade.cell(row=i+1,column=k).value,cpi.cell(row=i+1,column=2).value,weight.cell(row = j,column = k).value)
                     sum = sum + (grade.cell(row=i+1,column=k).value - cpi.cell(row=i+1,column=2).value) * weight.cell(row = j,column = k).value
 
         wsheet.cell(row=i+1,column=j).value = cpi.cell(row=i + 1, column=2).value + sum
 wb.save('data/prerequsite.xlsx')
 print('Grades are calculated and stored in "data/prerequsite.xlsx" file')
-# print('df')
